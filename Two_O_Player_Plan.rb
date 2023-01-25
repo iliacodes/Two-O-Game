be6bbed7d246fa65bdaab5eb8c1b7fd2d28dcbfe
iliacodes <6ac177ed@protonmail.com>
2023-01-24 16:01:26 -0800

    creating a plan for my game.

---
+++ Two_O_Player_Plan.rb
@@ -0,0 +1,23 @@
+# Player - Represents a single player in the game. Displays name, current score, remaining lives
+
+# Math Game - Logic for the game
+
+# Turns - Logic for tracking turns
+  # methods: nextTurn(), currentTurn()
+
+# Math addition problems - Logic for math question generation
+  # equationGenerator() 
+
+# Numbers (1-20) - The list of numbers that can be used to create math problems
+
+# Lives - Logic for tracking lives of each player
+  # methods: loselife()
+
+# Scores - Logic for tracking scores of each player
+  # methods: getscore()
+
+# Announcement - Logic for displaying announcement at the end of the game.
+  # methods: gameOver()
+
+
+
