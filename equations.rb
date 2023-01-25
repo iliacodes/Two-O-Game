2b33349292bebd26e3730b169510d87a33d58e0f
iliacodes <6ac177ed@protonmail.com>
2023-01-24 16:51:00 -0800

    changing the file to include two equations

--- equations.rb
+++ equations.rb
@@ -1,11 +1,12 @@
 require 'pp'
 require 'byebug'
 
-class MathProblems
+class MathProblem
   def self.generate_question
-    num1=rand(1..20)
-    num2=rand(1..20)
+    @num1=rand(1..20)
+    @num2=rand(1..20)
 
-    [num1, num2]
+    @sum = @num1 + @num2
+    @subtract = @num1 - @num2
   end
 end
